af243b9980238e31b015e48c9eab20d0d840fac4
Sejal Rajurkar <sejalrajurkar66@gmail.com>
2026-01-21 18:21:36 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lovely-flamingo-139.convex.cloud'
+      },
+      {
+        protocol: 'https',
+        hostname: 'sleek-capybara-771.convex.cloud'
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com'
+      }
+    ],
+    domains: ['resolute-chicken-7.convex.cloud'], 
+  },
+};
+
+export default nextConfig;
